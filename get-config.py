5af81ffef35788acdea1fe113b10aee6b99a059f
danyoung <dan.young@daisygroupplc.com>
2015-11-18 16:19:03 +0000

    get all files from config bucket

--- get-config.py
+++ get-config.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
+#
+# Get the config values from S3
+# Requires $TF_CONFIG_BUCKET env variable to be set
+#
+# dan.young@daisygroup.com
 
 import os
 import boto3
 
 s3_client = boto3.client('s3')
 
-# Download the file from S3
-s3_client.download_file(os.environ['TF_CONFIG_BUCKET'], 'terraform.tfvars', 'terraform.tfvars')
+bucket = os.environ['TF_CONFIG_BUCKET'];
+
+# Create a list of objects
+list = s3_client.list_objects(Bucket=bucket)['Contents']
+
+# Download the objects from S3
+for key in list:
+    s3_client.download_file(os.environ['TF_CONFIG_BUCKET'], key['Key'], key['Key'])
