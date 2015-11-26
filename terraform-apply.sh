436fb7e6b5497bb2769e6b9205c3a6441025aaea
danyoung <dan.young@daisygroupplc.com>
2015-11-26 12:44:12 +0000

    removed push to github from CircleCI

--- terraform-apply.sh
+++ terraform-apply.sh
@@ -3,7 +3,7 @@ echo "Checking..."
 if [ -a terraform.plan ]
   then
     echo "terraform.plan exists."
-    terraform apply terraform.plan
+    ./terraform apply terraform.plan
 fi
 echo
 echo "...done."
