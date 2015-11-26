a4aac7b7ca1c42972569bc1e22040c8333743131
danyoung <dan.young@daisygroupplc.com>
2015-11-26 12:21:29 +0000

    added terraform apply wrapper

--- terraform-apply.sh
+++ terraform-apply.sh
@@ -1,6 +1,6 @@
 echo "If the plan exists, then apply it"
 echo "Checking..."
-if [ -f terraform.plan ]
+if [ -a terraform.plan ]
   then
     echo "terraform.plan exists."
     ./terraform apply terraform.plan
